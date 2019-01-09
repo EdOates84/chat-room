d6dacd4cab0b57ae578c5be360f5d37b121d17bb
Nitesh84 <niteshmahawer1@gmail.com>
2019-01-09 23:48:30 +0530

    Deleted chat folder

--- mchat/mchat/routing.py
+++ mchat/mchat/routing.py
@@ -3,7 +3,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 import nchat.routing
 
 application = ProtocolTypeRouter({
-    # (http->django views is added by default)
+    
     'websocket': AuthMiddlewareStack(
         URLRouter(
             nchat.routing.websocket_urlpatterns
